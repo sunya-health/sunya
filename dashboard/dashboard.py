7d8a270f8a7ae6752165d852e2b7b87084997408
Bibek Chaudhary <bbkchdhry@gmail.com>
2019-08-29 18:29:14 +0545

    checked if user is superuser or not

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from . import context_processors
+from account.models import User
 # Create your views here.
 
 from django.views import View
@@ -9,6 +10,8 @@ class Dashboard(View):
     def get(self, request):
 
         if 'user' in request.session:
+            is_superuser = User.objects.values_list("is_superuser").filter(id=int(request.session['user']))[0][0]
+            print("is superuser: %s" % is_superuser)
             context = context_processors.base_variables_all(request)
             return render(request, 'dashboard.html', context)
         else:
